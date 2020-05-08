867face030f8d93ccdedf34790aee2ca265e8964
Oscar Nunez <taw.oscarn@gmail.com>
2020-05-08 03:18:52 -0700

    "convertido a redux"

---
+++ src/components/Counter.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { connect } from 'react-redux'
+import { sumar, restar, sumar2, restar2 } from '../redux/actions'
+class Counter extends React.Component {
+    state = {
+        countState: 0,
+    };
+    handleAddState = () => {
+        const { sumarRedux2 } = this.props
+        sumarRedux2()
+    };
+    handleLessState = () => {
+        const { restarRedux2 } = this.props
+        restarRedux2()
+    };
+    handleLessRedux = () => {
+        const { restarRedux } = this.props
+        restarRedux()
+    };
+    handleAddRedux = () => {
+        const { sumarRedux } = this.props
+        sumarRedux()
+    };
+    render() {
+        const { countRedux2 } = this.props;
+        const { countRedux } = this.props
+        console.log('render de countState: ', { state: this.state, props: this.props })
+        return (
+            <div>
+                <h1>Contador de redux 2</h1>
+                <p>{countRedux2}</p>
+                <button onClick={this.handleLessState}>-</button>
+                <button onClick={this.handleAddState}>+</button>
+                <br />
+                <h1>Contador de redux</h1>
+                <p>{countRedux}</p>
+                <button onClick={this.handleLessRedux}>-</button>
+                <button onClick={this.handleAddRedux}>+</button>
+            </div>
+        );
+    }
+}
+function mapStateToProps(state) {
+    return {
+        countRedux: state.count,
+        countRedux2: state.count2
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        sumarRedux: () => dispatch(sumar()),
+        restarRedux: () => dispatch(restar()),
+        sumarRedux2: () => dispatch(sumar2()),
+        restarRedux2: () => dispatch(restar2())
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
---
+++ src/redux/actions.js
@@ -0,0 +1,25 @@
+export function sumar() {
+    return {
+        type:'SUMAR'
+    }
+    
+}
+export function sumar2() {
+    return {
+        type:'SUMAR_COUNTER2'
+    }
+    
+}
+export function restar() {
+    return {
+        type:'RESTAR'
+    }
+    
+}
+export function restar2() {
+    return {
+        type:'RESTAR_COUNTER2'
+    }
+    
+}
+
